1f87afef14d11b85e2bc1bce542b83e1aa1392ec
Nauel Gómez <ngomez@codiara.com>
2025-10-20 22:48:36 +0200

    Callbacks + todo

--- examples/callbacks.js
+++ examples/callbacks.js
@@ -6,10 +6,44 @@ function suma(a, b, callback) {
     return resultado;
 };
 
-const resultado = suma(2, 5, function(res) {
-    console.log("Resultado: ", res);
-});
+//const resultado = suma(2, 5, function(res) {
+//    console.log("Resultado: ", res);
+// });
 
 // Cuando se ejecute suma, pinta el resultado por consola;
 
-console.log(resultado);
+//console.log(resultado);
+
+// Hacer una función que reciba un texto y tras dos segundos lo escriba en la consola.
+
+function escribeTrasDosSegundos(texto, callback) {
+    setTimeout(() => {
+        console.log(texto);
+        callback();
+    }, 2000);
+};
+
+
+
+// Llamarla dos veces (texto1 y texto2. Deben salir los textos con sus pausas correspondientes.
+
+escribeTrasDosSegundos('Texto 1', function () {
+    escribeTrasDosSegundos('Texto 2', function () {
+        escribeTrasDosSegundos('Texto 3', function () {
+            escribeTrasDosSegundos('Texto 4', function () {
+                escribeTrasDosSegundos('Texto 5', function () {
+                    console.log('Fin!');
+                });
+            });
+        });
+    });
+});
+
+// Y si me lo piden 5 veces?
+
+
+// TODO:
+// Repitamos la llamada a escribeTras2Segundos varias veces ejecutar un total de N veces?
+// Es posible con callbacks?
+
+
