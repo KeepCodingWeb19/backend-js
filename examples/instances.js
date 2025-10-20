8a9d939998a6be6f1e5cd01894f11a8aca7e86df
Nauel Gómez <ngomez@codiara.com>
2025-10-20 22:17:53 +0200

    Instancias

---
+++ examples/instances.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function Fruta(nombre) {
+    this.nombre = nombre;
+    // return this; // Lo añade JS al instanciar con new
+    this.saluda = function() {
+        console.log("Hola soy ", this.nombre);
+    }
+};
+
+const limon = new Fruta('limón');
+
+console.log(limon);
+limon.saluda();
