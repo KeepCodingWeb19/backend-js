8c5e75dbc4be4917187d5c54f3816e44a0103b43
Nauel Gómez <ngomez@codiara.com>
2025-10-21 21:15:44 +0200

    Listening exit on process

--- examples/exit-codes.js
+++ examples/exit-codes.js
@@ -2,7 +2,12 @@
 
 const process = require('node:process');
 
+process.on('exit', (code) => {
+  console.log('Process exit event with code: ', code);
+});
+
 setTimeout(() => {
-    process.exit(0); // En este caso entiende que ha finalizaado bien.
+    console.log("Exit call...")
+    // process.exit(0); // En este caso entiende que ha finalizaado bien.
     process.exit(10); // Si el código de error es diferente de 0 el sistema entiende que es un error.
 }, 1000);
