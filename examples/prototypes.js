99e1056158cea6ff0fd82cefd369df34a40bff83
Nauel Gómez <ngomez@codiara.com>
2025-10-21 20:23:20 +0200

    Prototype chain js

--- examples/prototypes.js
+++ examples/prototypes.js
@@ -1,6 +1,8 @@
 'use strict';
 
 function Persona(nombre) {
+    this.activo = true;
+    this.fechaAlta = new Date();
     this.nombre = nombre;
     // this.saluda = function() {
     //     console.log("Hola!! soy ", this.nombre);
@@ -18,3 +20,34 @@ pepe.saluda();
 const maria = new Persona('Maria');
 console.log(maria);
 maria.saluda();
+
+// Crear un tipo Estudiante, que herede de pesona.
+// class Estudiante extends Persona
+function Estudiante(nombre) {
+    Persona.call(this, nombre) // super(nombre)
+};
+Object.setPrototypeOf(Estudiante.prototype, Persona.prototype);
+
+const pedro = new Estudiante('Pedro');
+console.log(pedro);
+pedro.saluda();
+
+const sonia = new Estudiante('Sonia');
+console.log(sonia);
+sonia.saluda();
+
+// Class Estudiante extends Jugador, Persona
+
+function Jugador() {
+    this.chuta = function() {
+        console.log("He chutado!");
+    }
+};
+Object.assign(Estudiante.prototype, new Jugador());
+
+pedro.chuta();
+
+console.log(Estudiante.prototype);
+console.log(pedro instanceof Estudiante);
+console.log(pedro instanceof Persona);
+console.log(pedro instanceof Jugador);
