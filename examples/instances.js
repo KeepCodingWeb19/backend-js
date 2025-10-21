d5e3b75cbfd8b23677686a94aefa9d6f4a5b654e
Nauel Gómez <ngomez@codiara.com>
2025-10-21 19:43:05 +0200

    This concept & arrow functions

--- examples/instances.js
+++ examples/instances.js
@@ -3,7 +3,10 @@
 function Fruta(nombre) {
     this.nombre = nombre;
     // return this; // Lo añade JS al instanciar con new
-    this.saluda = function() {
+    // this.saluda = function() {
+    //     console.log("Hola soy ", this.nombre);
+    // }
+    this.saluda = () => {
         console.log("Hola soy ", this.nombre);
     }
 };
@@ -12,3 +15,8 @@ const limon = new Fruta('limón');
 
 console.log(limon);
 limon.saluda();
+
+setTimeout(limon.saluda.bind(limon), 2000);
+
+const funcionSaludaDeLimon = limon.saluda;
+funcionSaludaDeLimon(); // limon.funcionSaludaDeLimon();
