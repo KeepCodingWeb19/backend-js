7399edf98a7e94dd9aeeb8dceb24ff2f84dd701d
Nauel Gómez <ngomez@codiara.com>
2025-10-27 19:42:52 +0100

    TODO: get all completed filter done

--- node-api/controllers/todoController.js
+++ node-api/controllers/todoController.js
@@ -6,6 +6,20 @@ export const todoController = {
 
         let retTodos = todos;
 
+        if ( req.query.completed ) {
+            // ?completed=true
+            // ?completed=false
+            // ?completed=1
+            // ?completed=0
+            // const completed = req.query.completed === 'true';
+            const completed = 
+                req.query.completed.toLowerCase() === 'true' ||
+                req.query.completed === '1';
+            // Cuando es false quiere los no completados o los quiere todos?
+            
+            retTodos = retTodos.filter(i => i.completed === completed);
+        }
+
         if ( req.query.limit || req.query.skip ) {
             const limit = parseInt(req.query.limit);
             const skip = parseInt(req.query.skip) || 0;
