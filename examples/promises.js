0cecf733cd1a28f31c813004658c28dc9b8ff958
Nauel Gómez <ngomez@codiara.com>
2025-10-28 19:48:51 +0100

    promises 2

--- examples/promises.js
+++ examples/promises.js
@@ -12,3 +12,23 @@ saluda.then(res => {
 .catch(err => {
     console.error('Fatal Error', err);
 });
+
+// TODO: Crea una promesa de azar con un 20% de posibilidades de fallar
+// y devolver el valor en 2 segundos
+const azar = new Promise((res, rej) => {
+    setTimeout(() => {
+        const rnd = Math.random();
+        if (rnd <= 0.2) {
+            rej(`Azar falló con: ${rnd.toFixed(2)}`);
+        } else {
+            res(`Azar OK con: ${rnd.toFixed(2)}`);
+        }
+    }, 2000);
+});
+
+azar.then(res => {
+    console.log(res);
+})
+.catch(err => {
+    console.error(err);
+})
