f5ab71719e056222188e1af4b40745cd61e7da8e
Nauel Gómez <ngomez@codiara.com>
2025-10-28 20:17:14 +0100

    Promises async await

--- examples/promises.js
+++ examples/promises.js
@@ -33,19 +33,31 @@ azar.then(res => {
     console.error(err);
 });
 
-function paso1() {
-    return new Promise((res, rej) => {
-        res('Paso 1 resuelto');
-    });
+// function paso1() {
+//     return new Promise((res, rej) => {
+//         res('Paso 1 resuelto');
+//     });
+// };
+
+async function paso1() {
+    return 'Paso 1 resuelto';
 };
 
-function paso2() {
-    return Promise.resolve('Paso 2 resuelto');
+// function paso2() {
+//     return Promise.resolve('Paso 2 resuelto');
+// };
+
+async function paso2() {
+    return 'Paso 2 resuelto';
 };
 
 function paso3() {
     return Promise.reject('Paso 3 fallo');
 };
+
+// async function paso3() {
+//     return 'Paso 3 fallo';
+// };
 
 // paso1()
 //     .then(res => {
@@ -87,8 +99,35 @@ Promise.race([
 })
 .catch(err => {
     printError(err);
-})
+});
 
+
+async function ejecutaPasos() {
+    try {
+        const res1 = await paso1();
+        console.log(res1);
+        
+        const res2 = await paso2();
+        console.log(res2);
+        
+        const res3 = await paso3();
+        console.log(res3);
+    } catch(ex) {
+        printError(ex);
+    }
+    
+}
+ejecutaPasos();
+
+// Saluda 3 veces
+async function saluda5Veces() {
+    // No es lo mismo un bucle for que un [..].map()
+    for (let i = 0; i < 5; i++) {
+        const msg = await paso1();
+        console.log(msg);
+    }
+};
+saluda5Veces();
 
 
 
