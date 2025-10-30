f8fac23c63324a3bb4c1d4074d73e66058a50c00
Nauel Gómez <ngomez@codiara.com>
2025-10-30 20:07:38 +0100

    Seed db finished

--- node-api/seedDB.js
+++ node-api/seedDB.js
@@ -1,16 +1,21 @@
+import readline from 'node:readline/promises';
+
 import { connectMongoose } from './lib/connectMongoose.js';
+import { Agent } from './models/Agent.js';
 import { User } from './models/User.js';
 
 const connection = await connectMongoose();
 console.log(`Connected to MongoDB: ${connection.name}`);
 
-const AGENTS = [
-    { name: 'Smith', age: 45, owner: undefined },
-    { name: 'Brown', age: 33, owner: undefined },
-    { name: 'Jones', age: 24, owner: undefined },
-];
+// Check de seguridad
+const answer = await ask('Estas seguro que deseas eliminar toda la información de la base de datos? [y/N] ');
+if (answer.toLowerCase() !== 'y') {
+    console.log('Script abortado');
+    process.exit(0);
+}
 
 await seedUsers();
+await seedAgents();
 
 await connection.close();
 process.exit(0);
@@ -34,3 +39,36 @@ async function seedUsers() {
 
 // TODO: Crea una función para inicializar los agentes.
 // Los dos primeros senrán propiedad de admin y el tercero de user.
+async function seedAgents() {
+
+    // Delete all agents
+    const deleteResult = await Agent.deleteMany();
+    console.log(`Deleted ${deleteResult.deletedCount} Agents`);
+
+    const [user, admin] = await Promise.all([
+        User.findOne({ email: 'user@example.com' }),
+        User.findOne({ email: 'admin@example.com' })
+    ]);
+
+     const AGENTS = [
+        { name: 'Smith', age: 45, owner: admin._id },
+        { name: 'Brown', age: 33, owner: admin._id },
+        { name: 'Jones', age: 24, owner: user._id },
+    ];
+
+    const insertResult = await Agent.insertMany(AGENTS);
+
+    console.log(`Inserted ${insertResult.length} Agents`);
+
+}
+
+// Función para preguntar por consola
+async function ask(question) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    const result = await rl.question(question);
+    rl.close();
+    return result;
+}
